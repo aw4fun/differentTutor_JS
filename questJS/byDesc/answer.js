2e29412209bdfa81123d553335179d692f3c2951
Andrey Belokon <aw4fun@yandex.ru>
2020-01-06 22:07:21 +0300

    quest_byDesc

---
+++ questJS/byDesc/answer.js
@@ -0,0 +1,2 @@
+const byDesc = arr => arr.sort((a, b) => b - a);
+console.log(byDesc([3, 10, 2, 7]));
---
+++ questJS/byDesc/index.js
@@ -0,0 +1,4 @@
+function byDesc(arr) {
+    return arr.sort((a, b) => b > a);
+}
+console.log(byDesc([5, 3, 2, 8, 1, 7 ,9]));
