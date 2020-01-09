99d41bd58f639c5060e336b7cac0f8ae32ad2181
Andrey Belokon <aw4fun@yandex.ru>
2020-01-09 20:32:02 +0300

    quest_nearestDistance

--- questJS/nearestDistance/index.js
+++ questJS/nearestDistance/index.js
@@ -1,14 +1,21 @@
 function nearestDistance(point, arr) {
-    
+    let tmp = [];
+        arr.map((item, idx) => {
+        tmp.push({
+            idx,
+            weight: Math.max(Math.abs(item.x - point.x), Math.abs(item.y - point.y))});
+    });
+        tmp.sort((a, b) => a.weight - b.weight);
+    return tmp[0].idx;
 }
-
-console.log(nearestDistance({x:0, y:0}, arr));
 
 let arr = [
     {x:10, y:10},
     {x:2, y:5},
     {x:3, y:4}
 ];
+
+console.log(nearestDistance({x:4, y:6}, arr));
 
 let pattern = [
     'X',0 ,0 ,0 ,0,0,0,0,0, 0,
