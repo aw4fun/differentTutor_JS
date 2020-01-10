04ff112b0fa622c2869831e8f1834f1a96a55395
Andrey Belokon <aw4fun@yandex.ru>
2020-01-10 21:22:50 +0300

    quest_bmi

---
+++ questJS/bmi/answer.js
@@ -0,0 +1,2 @@
+const bmi = (m, h) => +(m / (h ** 2)).toFixed(2);
+console.log(bmi(100, 1.8));
---
+++ questJS/bmi/index.js
@@ -0,0 +1,4 @@
+function bmi(m, h) {
+    return m / Math.pow(h,2);
+}
+console.log(bmi(100, 1.8).toFixed(2));
