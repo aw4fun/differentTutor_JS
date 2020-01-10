df529694a1019b7e52e4a2214e1fe4f5763c7327
Andrey Belokon <aw4fun@yandex.ru>
2020-01-10 22:23:10 +0300

    quest_fibbo_trcursion ugly way

--- questJS/fib/index.js
+++ questJS/fib/index.js
@@ -1,17 +1,38 @@
 
-// function fibbo(n) {
-//     console.log(n);
-//     if (n === 0) return 0;
-//     if (n === 1) return 1;
-//    return fib(n-1) + fib(n-2);
-// }
+function decorator(num) {
+    const memo = {};
+    const answer = [];
+    function fibbo(num) {
+
+        if (memo[num]) return memo[num];
+        if (num === 0) {
+            memo[num] = 0;
+            return memo[num];
+        };
+        if (num === 1){
+            memo[num] = 1;
+            return memo[num];
+        };
+
+        return memo[num] = fibbo(num-1) + fibbo(num-2);
+    };
+    fibbo(num);
+    console.log(memo);
+    for (let key in memo) {
+        answer.push(memo[key]);
+    }
+    return answer;
+}
 // have some problem =\
+console.log(decorator(4));
 
+// by recursion ^ ugly >.<
 
+// by loop v
 
 function fib(n) {
     const tmp = [];
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i <= n; i++) {
         if (i === 0) {
             tmp.push(0);
             continue};
@@ -23,4 +44,4 @@ function fib(n) {
     return tmp;
 }
 
-console.log(fib(6));
+console.log(fib(4));
